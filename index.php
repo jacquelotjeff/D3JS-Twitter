480598ac1d06a9950f41ce862a275a8a1eeb3a2a
jacquelotjeff <jacquelotjeff@gmail.com>
2017-04-04 18:41:23 +0200

    Add connection files

--- index.php
+++ index.php
@@ -4,7 +4,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$contentParams = file_get_contents('config/params.json');
+$params        = json_decode($contentParams, true);
+
+$nbTweets = $params['total_tweets'];
+$subject  = $params['subject'];
+
 require 'vendor/autoload.php';
+require 'database/connection.php';
+
 include "oauth.php";
 
 ?>
--- oauth.php
+++ oauth.php
@@ -5,32 +5,30 @@ use GuzzleHttp\Subscriber\Oauth\Oauth1;
 
 $client = new Client(
     ['base_url' => 'https://api.twitter.com',
-    'defaults' => ['auth' => 'oauth']
-]);
-$json = file_get_contents('twitter.conf.json');
+     'defaults' => ['auth' => 'oauth'],
+    ]);
+$json   = file_get_contents('twitter.conf.json');
 
 $stack = \GuzzleHttp\HandlerStack::create();
 
-$authData = json_decode($json, true);
+$authData   = json_decode($json, true);
 $middleware = new Oauth1($authData);
 
 $stack->push($middleware);
 
 $client = new Client([
     'base_uri' => 'https://api.twitter.com/1.1/',
-    'handler' => $stack
+    'handler'  => $stack,
 ]);
 
-// Set the "auth" request option to "oauth" to sign using oauth
-$res = $client->get('statuses/home_timeline.json', ['auth' => 'oauth']);
-
-//var_dump($res);
 
 /*
  * Executing a GET request on the timeline service, pass the result to the json parser
  */
 
-$res = $client->get('search/tweets.json', ['verify'=>'/etc/pki/tls/certs/ca-bundle.crt'])->json();
+$toSearch = urlencode('imagine Dragons');
+$maxTweets = 100;
 
-
+//$res = $client->get('search/tweets.json?count='.$maxTweets.'&q='.$toSearch, ['auth' => 'oauth']);
+//var_dump($res->getBody()->getContents()); die;
 print_r($res); //we have the parsed response in an array!
