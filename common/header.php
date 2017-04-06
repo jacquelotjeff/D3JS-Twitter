15f96051e5e8b59bc9e52a6ebdc3c5bd5f88cf30
jacquelotjeff <jacquelotjeff@gmail.com>
2017-04-06 16:03:32 +0200

    Add theme bootstrap

---
+++ common/header.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <title></title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/sb-admin.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+</head>
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">D4JS with Twitter & Mongo DB</a>
+            </div>
+
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li class="active">
+                        <a href="#"><i class="fa fa-fw fa-dashboard"></i> Paris</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
--- index.php
+++ index.php
@@ -15,11 +15,9 @@ $tweetsOnTime      = getTweetsOnTime($manager);
 $hashtags          = getHashtags($manager);
 ?>
 
-<!doctype html>
-<html lang="fr">
-    <head>
-    </head>
-    <body>
+<?php
+    include 'common/header.php';
+?>
         <h1>Big data</h1>
         <h2>Sujets entourant Paris</h2>
         <div style="margin-left: 100px;width:500px; height:500px">
@@ -37,22 +35,7 @@ $hashtags          = getHashtags($manager);
         <div style="width: 700px; height: 700px; position: absolute;">
             <canvas id="tweetsOnTime"></canvas>
         </div>
-        <script>
-            var tweetsByCountry = <?php echo json_encode($tweetsByCountries) ?>;
-            var tweetsOnTime = <?php echo json_encode($tweetsOnTime) ?>;
-            var hashtags = <?php echo json_encode($hashtags); ?>;
-        </script>
-        <script src="js/utils/colors.js"></script>
-        <script
-                src="https://code.jquery.com/jquery-3.2.1.min.js"
-                integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-                crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
-        <script type="text/javascript" src="js/tagcloud.js"></script>
-        <script src="js/tweets-on-time.js" charset="UTF-8"></script>
-        <script src="js/hastags-cloud.js" charset="UTF-8"></script>
-        <script src="js/most-retweeted.js" charset="UTF-8"></script>
-        <script src="js/tagcloud.js" charset="UTF-8"></script>
-        <script src="js/tweets-by-country.js" charset="UTF-8"></script>
-    </body>
-</html>
+
+<?php
+include 'common/footer.php';
+?>
